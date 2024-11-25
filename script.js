02aacc28355e667b93b0049d4b313455e0133b64
claraharguindey <clarisaharguindey@gmail.com>
2024-11-25 22:57:20 +0100

    Refactor script

--- script.js
+++ script.js
@@ -1,73 +1,21 @@
-const explosion = new Audio("https://www.fesliyanstudios.com/play-mp3/561");
-const cristales = new Audio("https://www.fesliyanstudios.com/play-mp3/5659");
-const perro = new Audio(
-  "dog-bark.mp3"
-);
-const risa = new Audio(
-  "laughter3.mp3"
-);
-const nena = new Audio(
-  "nena.mp3"
-);
-
-const ramala = new Audio(
-  "ramala.mp3"
-);
-
-const buttonCristales = document.querySelector("#cristales");
-const buttonExplosion = document.querySelector("#explosion");
-const buttonPerro = document.querySelector("#perro");
-const buttonRisa = document.querySelector("#risa");
-const buttonRamala = document.querySelector("#ramala");
-const buttonNena = document.querySelector("#nena");
-
-const stopAll = () => {
-  cristales.pause();
-  explosion.pause();
-  perro.pause();
-  risa.pause();
-  ramala.pause();
-  nena.pause();
+const audios = {
+  cristales: new Audio("https://www.fesliyanstudios.com/play-mp3/5659"),
+  explosion: new Audio("https://www.fesliyanstudios.com/play-mp3/561"),
+  perro: new Audio("dog-bark.mp3"),
+  risa: new Audio("laughter3.mp3"),
+  ramala: new Audio("ramala.mp3"),
+  nena: new Audio("nena.mp3"),
 };
 
-buttonCristales?.addEventListener("click", () => {
-  stopAll();
-  cristales.play();
-});
+const stopOthers = (current) => {
+  Object.values(audios).forEach((audio) => {
+    if (audio !== current) audio.pause();
+  });
+};
 
-buttonExplosion?.addEventListener("click", () => {
-  stopAll();
-  explosion.play();
-});
-
-buttonPerro?.addEventListener("click", () => {
-  stopAll();
-  perro.play();
-});
-
-buttonRisa?.addEventListener("click", () => {
-  stopAll();
-  risa.play();
-});
-
-buttonRamala?.addEventListener("click", () => {
-  stopAll();
-  ramala.play();
-});
-
-buttonNena?.addEventListener("click", () => {
-  stopAll();
-  nena.play();
-});
-
-document.addEventListener("keydown", function (e) {
-  const key = e.key.toLowerCase();
-  const button = document.querySelector(`button.${key}`);
-  button.classList.add("active");
-});
-
-document.addEventListener("keyup", function (e) {
-  const key = e.key.toLowerCase();
-  const button = document.querySelector(`button.${key}`);
-  button.classList.remove("active");
+Object.keys(audios).forEach((key) => {
+  document.querySelector(`#${key}`)?.addEventListener("click", () => {
+    const audio = audios[key];
+    audio.paused ? (stopOthers(audio), audio.play()) : audio.pause();
+  });
 });
